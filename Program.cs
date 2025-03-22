fc759694cfef750a50b10c35c8eab0b611e6eadb
Calderaly <calderaly16@gmail.com>
2025-03-23 02:45:26 +0700

    feat(Design by Contract): Implementasi Design by Contract

--- Program.cs
+++ Program.cs
@@ -6,14 +6,36 @@
 
     public SayaTubeVideo(string? title)
     {
+        if (title == null) throw new ArgumentNullException("Judul Video", "Judul video tidak boleh kosong");
+
+        if (title.Length > 100) throw new ArgumentException("Judul video tidak boleh lebih dari 100 karakter");
+
+        Random rand = new Random();
+        this.id = rand.Next(10000, 99999);
         this.title = title;
-        this.id = Random.Shared.Next(10000, 100000);
         this.playCount = 0;
     }
 
     public void IncreasePlayCount(int playCount)
     {
-        this.playCount += playCount;
+
+        if (playCount > 10000000)
+        {
+            throw new ArgumentOutOfRangeException("Play Count", "Penambahan playCount maksimal 10.000.000 per panggilan.");
+        }
+
+        try
+        {
+
+            checked
+            {
+                this.playCount += playCount;
+            }
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Error: Terjadi overflow pada playCount.");
+        }
     }
 
     public void PrintVideoDetails()
@@ -28,8 +50,23 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design by Contract - Caldera S");
-        video.IncreasePlayCount(5);
-        video.PrintVideoDetails();
+        try
+        {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Caldera S");
+            video.IncreasePlayCount(15000000);
+            video.PrintVideoDetails();
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
     }
 }
